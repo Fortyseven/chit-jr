5b750e6b70bc8071d940bf039f46f07a81366935
Fortyseven <fortyseven@network47.org>
2024-07-05 05:15:27 -0400

    feat: adds "retort" prompt (this is a bad idea)

--- sidebar/prompts/index.js
+++ sidebar/prompts/index.js
@@ -2,8 +2,17 @@ import analysis from "./analysis.js";
 import translate from "./translate.js";
 import sdprompt from "./sdprompt.js";
 import summarize from "./summarize.js";
+import extract_and_infer from "./extract-and-infer.js";
+import retort from "./retort.js";
 
-const SYSTEM_PROMPTS = { summarize, translate, sdprompt, analysis };
+const SYSTEM_PROMPTS = {
+    summarize,
+    translate,
+    sdprompt,
+    analysis,
+    extract_and_infer,
+    retort,
+};
 
 window.SYSTEM_PROMPTS = SYSTEM_PROMPTS;
 
---
+++ sidebar/prompts/retort.js
@@ -0,0 +1,17 @@
+export default {
+    name: "🤡 Retort",
+    prompt: `Please compose a scathing retort to the provided block of text.
+
+Compose a concise and pointed response that highlights the flaws, inaccuracies, or logical fallacies in the original argument.
+
+Your retort should be well-reasoned, persuasive, and backed up with evidence or examples.
+
+Focus on the content of the text and the strength of the argument itself.
+
+The retort should be clear, direct, and compelling.
+
+A decent amount of humor is encouraged.
+
+Text begins:`,
+    temperature: 1.2,
+};
